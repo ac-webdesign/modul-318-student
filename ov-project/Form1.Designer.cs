b46cab13a9390e1e31000b03834e703dd56761f6
AC-BYOD <adrian.cerdeira@viag.ch>
2018-11-28 14:21:20 +0100

    Dresign angepasst

--- ov-project/Form1.Designer.cs
+++ ov-project/Form1.Designer.cs
@@ -164,7 +164,7 @@
             // 
             this.labelDate.AutoSize = true;
             this.labelDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDate.Location = new System.Drawing.Point(351, 60);
+            this.labelDate.Location = new System.Drawing.Point(351, 56);
             this.labelDate.Name = "labelDate";
             this.labelDate.Size = new System.Drawing.Size(80, 16);
             this.labelDate.TabIndex = 2;
